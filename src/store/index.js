3864bef46be21b6d33999adee18846f0310804be
Rob Leroy <robleroy@msn.com>
2021-01-11 08:59:58 -0600

    update

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [],
+  },
+  getters: {
+    todos(state) {
+      return state.todos;
+    }
   },
   mutations: {
+    setTodos(state, payload) {
+      state.todos = payload;
+    }
   },
   actions: {
+    async fetchTodos(commit, payload) {      
+      const res = await fetch("/.netlify/functions/pokemon");
+      const data = await res.json();
+      commit('setTodos', data)
+    }
   },
   modules: {
   }
